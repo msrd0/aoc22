e3e2601214ef88613cb15d50ca715d51f3ddce82
Dominic <git@msrd0.de>
2022-12-15 11:53:56 +0100

    day15 part 2

--- day15/main.rs
+++ day15/main.rs
@@ -8,21 +8,17 @@ use std::{
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Position {
-	x: i32,
-	y: i32
+	x: i64,
+	y: i64
 }
 
 impl Position {
-	fn new(x: i32, y: i32) -> Self {
-		Self { x, y }
-	}
-
 	fn parser() -> impl Parser<char, Self, Error = Simple<char>> + Clone {
 		let num = choice((
 			digits(10).map(|digits: String| digits.parse().unwrap()),
 			just("-")
 				.ignore_then(digits(10))
-				.map(|digits: String| -(digits.parse::<i32>().unwrap()))
+				.map(|digits: String| -(digits.parse::<i64>().unwrap()))
 		));
 		just("x=")
 			.ignore_then(num)
@@ -31,7 +27,7 @@ impl Position {
 			.map(|(x, y)| Self { x, y })
 	}
 
-	fn manhattan_dist(&self, other: &Self) -> u32 {
+	fn manhattan_dist(&self, other: &Self) -> u64 {
 		self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
 	}
 }
@@ -73,17 +69,21 @@ fn read<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<Vec<Sensor>> {
 	})
 }
 
-const LINE_Y: i32 = 2000000;
-const MAX: i32 = 4000000;
-const MIN: i32 = 0;
+// const LINE_Y: i64 = 10;
+// const MAX: i64 = 20;
+// const MIN: i64 = 0;
+
+const LINE_Y: i64 = 2000000;
+const MAX: i64 = 4000000;
+const MIN: i64 = 0;
 
 #[derive(Debug, Default)]
 struct Row {
-	ranges: Vec<RangeInclusive<i32>>
+	ranges: Vec<RangeInclusive<i64>>
 }
 
 impl Row {
-	fn add_range(&mut self, range: RangeInclusive<i32>) {
+	fn add_range(&mut self, range: RangeInclusive<i64>) {
 		for r in &mut self.ranges {
 			// start of the new range is included
 			if range.start() >= r.start() && range.start() <= r.end() {
@@ -105,9 +105,23 @@ impl Row {
 		self.ranges.push(range);
 	}
 
-	fn blocked(&self) -> u32 {
-		let mut blocked: u32 = 0;
-		let mut last_blocked: Option<i32> = None;
+	fn normalize(&mut self) {
+		self.ranges.sort_unstable_by_key(|range| *range.start());
+		let mut i = 0;
+		while i < self.ranges.len() - 1 {
+			if self.ranges[i].end() + 1 >= *self.ranges[i + 1].start() {
+				self.ranges[i] = *self.ranges[i].start()
+					..= *self.ranges[i].end().max(self.ranges[i + 1].end());
+				self.ranges.remove(i + 1);
+				continue;
+			}
+			i += 1;
+		}
+	}
+
+	fn blocked(&self) -> u64 {
+		let mut blocked: u64 = 0;
+		let mut last_blocked: Option<i64> = None;
 
 		for r in &self.ranges {
 			if let Some(last_blocked) = last_blocked {
@@ -115,35 +129,68 @@ impl Row {
 					continue;
 				}
 				let start = (last_blocked + 1).max(*r.start());
-				blocked += (r.end() - start + 1) as u32;
+				blocked += (r.end() - start + 1) as u64;
 			} else {
-				blocked = (r.end() - r.start() + 1) as u32;
+				blocked = (r.end() - r.start() + 1) as u64;
 			}
 			last_blocked = Some(*r.end());
 		}
 
 		blocked
 	}
+
+	fn find_free(&self) -> Vec<i64> {
+		let mut last = MIN - 1;
+		let mut free = Vec::new();
+		for r in &self.ranges {
+			if *r.start() > last + 1 {
+				free.push(last + 1);
+			}
+			last = *r.end();
+		}
+		free
+	}
 }
 
 #[derive(Default)]
 struct Map {
-	map: BTreeMap<i32, Row>
+	map: BTreeMap<i64, Row>
 }
 
 impl Map {
-	fn add_x_range(&mut self, range: RangeInclusive<i32>, y: i32) {
+	fn add_x_range(&mut self, range: RangeInclusive<i64>, y: i64) {
+		if range.start() > range.end() {
+			panic!("range={range:?}");
+		}
+
 		let entry = self.map.entry(y).or_default();
-		entry.add_range(range.clone());
-		entry.ranges.sort_unstable_by_key(|range| *range.start());
-		//eprintln!("{:?} (added {range:?})", entry);
+		entry.add_range(range);
+		entry.normalize();
 	}
 
-	fn blocked(&self, y: i32) -> u32 {
+	fn blocked(&self, y: i64) -> u64 {
 		self.map
 			.get(&y)
 			.map(|row| row.blocked())
 			.unwrap_or_default()
+	}
+
+	fn find_free(&self) -> Vec<Position> {
+		let mut positions = vec![];
+		for y in MIN ..= MAX {
+			if y % 100000 == 0 {
+				println!("find_free(): y={y}");
+			}
+
+			if let Some(row) = self.map.get(&y) {
+				for x in row.find_free() {
+					positions.push(Position { x, y });
+				}
+			} else {
+				eprintln!("Missing row for y={y}");
+			}
+		}
+		positions
 	}
 }
 
@@ -159,14 +206,31 @@ fn main() -> anyhow::Result<()> {
 
 		let beacon_dist = s.pos.manhattan_dist(&s.nearest_beacon);
 		let line_dist = s.pos.y.abs_diff(LINE_Y);
-		//println!(" - {s:?} (dist: {beacon_dist})");
 		if line_dist > beacon_dist {
 			continue;
 		}
-		let xrange = (beacon_dist - line_dist) as i32;
+		let xrange = (beacon_dist - line_dist) as i64;
 		map.add_x_range(s.pos.x - xrange ..= s.pos.x + xrange, LINE_Y);
 	}
-	println!("{}", map.blocked(LINE_Y) - beacons_in_line.len() as u32);
+	println!("{}", map.blocked(LINE_Y) - beacons_in_line.len() as u64);
+
+	// part 2
+
+	map = Map::default();
+	for s in &sensors {
+		println!(" - {s:?}");
+		let beacon_dist = s.pos.manhattan_dist(&s.nearest_beacon) as i64;
+		for y in (s.pos.y - beacon_dist).max(MIN) ..= (s.pos.y + beacon_dist).min(MAX) {
+			let xrange = beacon_dist - s.pos.y.abs_diff(y) as i64;
+			map.add_x_range(
+				(s.pos.x - xrange).max(MIN) ..= (s.pos.x + xrange).min(MAX),
+				y
+			);
+		}
+	}
+	for free in map.find_free() {
+		println!("{free:?} = {}", free.x * 4000000 + free.y);
+	}
 
 	Ok(())
 }
