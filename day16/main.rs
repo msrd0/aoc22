b524581e39c5585d988116df7e9fccd82b3990f6
Dominic <git@msrd0.de>
2022-12-16 16:16:38 +0100

    day16 part 2

--- day16/main.rs
+++ day16/main.rs
@@ -1,20 +1,19 @@
 use anyhow::anyhow;
+use bit_vec::BitVec;
 use chumsky::{
 	prelude::*,
 	text::{digits, ident}
 };
+use indexmap::IndexMap;
 use rayon::prelude::*;
-use std::{
-	collections::{BTreeSet, HashMap, HashSet},
-	fs
-};
+use std::{collections::HashMap, fs};
 
 struct Vertex {
 	flow_rate: u32,
 	adj: Vec<String>
 }
 
-fn parser() -> impl Parser<char, HashMap<String, Vertex>, Error = Simple<char>> {
+fn parser() -> impl Parser<char, IndexMap<String, Vertex>, Error = Simple<char>> {
 	let adj = ident()
 		.then_ignore(just(", "))
 		.repeated()
@@ -40,7 +39,7 @@ fn parser() -> impl Parser<char, HashMap<String, Vertex>, Error = Simple<char>> 
 		.map(|vec| vec.into_iter().collect())
 }
 
-fn read<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<HashMap<String, Vertex>> {
+fn read<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<IndexMap<String, Vertex>> {
 	parser().parse(fs::read_to_string(path)?).map_err(|errors| {
 		anyhow!(errors
 			.into_iter()
@@ -50,51 +49,76 @@ fn read<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<HashMap<String, Ve
 	})
 }
 
-#[derive(Clone, Eq, Hash, PartialEq)]
-struct State {
-	vertex: String,
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct State<'a> {
+	vertex: &'a str,
 	flow_rate: u32,
-	pressure: u32,
-	open: BTreeSet<String>
+	open: BitVec
 }
 
-fn bfs(
-	vertices: &HashMap<String, Vertex>,
+impl State<'_> {
+	fn is_open(&self, idx: usize) -> bool {
+		self.open.get(idx).unwrap_or(false)
+	}
+
+	fn set_open(&mut self, idx: usize) {
+		if self.open.len() <= idx {
+			self.open.grow(idx - self.open.len() + 1, false);
+		}
+		self.open.set(idx, true);
+	}
+}
+
+fn bfs<'a>(
+	vertices: &'a IndexMap<String, Vertex>,
 	max_open_vertices: usize,
-	mut q: HashSet<State>,
+	mut q: HashMap<State<'a>, u32>,
 	mut remaining: u32
-) -> HashSet<State> {
+) -> HashMap<State<'a>, u32> {
+	let init_q_len = q.len();
 	while remaining > 0 {
+		let q_len = q.len();
 		println!(" remaining: {remaining}, q: {}", q.len());
+
 		q = q
 			.into_par_iter()
-			.map(|mut state| {
-				let mut next = HashSet::new();
+			.map(|(state, mut pressure)| {
+				let mut next = HashMap::new();
 
-				state.pressure += state.flow_rate;
+				pressure += state.flow_rate;
 				if state.open.len() == max_open_vertices {
-					next.insert(state);
+					next.insert(state, pressure);
 					return next;
 				}
 
-				let vertex = &vertices[&state.vertex];
-				if !state.open.contains(&state.vertex) && vertex.flow_rate > 0 {
+				if init_q_len != q_len && state.vertex == "AA" && state.flow_rate == 0 {
+					//eprintln!("skipping {state:?}");
+					return next;
+				}
+
+				let (vertex_idx, _, vertex) = vertices.get_full(state.vertex).unwrap();
+				if !state.is_open(vertex_idx) && vertex.flow_rate > 0 {
 					let mut state = state.clone();
-					state.open.insert(state.vertex.clone());
+					state.set_open(vertex_idx);
 					state.flow_rate += vertex.flow_rate;
-					next.insert(state);
+					let entry = next.entry(state).or_default();
+					*entry = pressure.max(*entry);
 				}
 
 				for v in &vertex.adj {
 					let mut state = state.clone();
-					state.vertex = v.to_owned();
-					next.insert(state);
+					state.vertex = v;
+					let entry = next.entry(state).or_default();
+					*entry = pressure.max(*entry);
 				}
 
 				next
 			})
-			.reduce(HashSet::new, |mut acc, other| {
-				acc.extend(other.into_iter());
+			.reduce(HashMap::new, |mut acc, other| {
+				for (state, pressure) in other {
+					let entry = acc.entry(state).or_default();
+					*entry = pressure.max(*entry);
+				}
 				acc
 			});
 		remaining -= 1;
@@ -106,44 +130,44 @@ fn main() -> anyhow::Result<()> {
 	let vertices = read("input.txt")?;
 	let max_open_vertices = vertices.values().filter(|v| v.flow_rate > 0).count();
 
-	let mut q = HashSet::new();
-	q.insert(State {
-		vertex: "AA".into(),
-		flow_rate: 0,
-		pressure: 0,
-		open: Default::default()
-	});
+	let mut q = HashMap::new();
+	q.insert(
+		State {
+			vertex: "AA",
+			flow_rate: 0,
+			open: Default::default()
+		},
+		0
+	);
 	let q = bfs(&vertices, max_open_vertices, q, 30);
-	let max = q.par_iter().map(|state| state.pressure).max().unwrap();
+	let max = q.par_iter().map(|(_, pressure)| pressure).max().unwrap();
 	println!("{max}");
 
 	// part 2
 
-	let mut q = HashSet::new();
-	q.insert(State {
-		vertex: "AA".into(),
-		flow_rate: 0,
-		pressure: 0,
-		open: Default::default()
-	});
+	let mut q = HashMap::new();
+	q.insert(
+		State {
+			vertex: "AA",
+			flow_rate: 0,
+			open: Default::default()
+		},
+		0
+	);
 	let q = bfs(&vertices, max_open_vertices, q, 26);
 
 	let mut elephant_q = HashMap::new();
-	for state in q {
-		let value: &mut u32 = elephant_q.entry(state.open).or_default();
-		*value = state.pressure.max(*value);
-	}
-	let elephant_q = elephant_q
-		.into_iter()
-		.map(|(open, pressure)| State {
-			vertex: "AA".into(),
+	for (state, pressure) in q {
+		let key = State {
+			vertex: "AA",
 			flow_rate: 0,
-			pressure,
-			open
-		})
-		.collect();
+			open: state.open
+		};
+		let value: &mut u32 = elephant_q.entry(key).or_default();
+		*value = pressure.max(*value);
+	}
 	let q = bfs(&vertices, max_open_vertices, elephant_q, 26);
-	let max = q.par_iter().map(|state| state.pressure).max().unwrap();
+	let max = q.par_iter().map(|(_, pressure)| pressure).max().unwrap();
 	println!("{max}");
 
 	Ok(())
