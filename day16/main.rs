089c38386520abd5261890847a3670ffd932517e
Dominic <git@msrd0.de>
2022-12-16 14:29:08 +0100

    day16 part 1

--- day16/main.rs
+++ day16/main.rs
@@ -3,9 +3,9 @@ use chumsky::{
 	prelude::*,
 	text::{digits, ident}
 };
+use indexmap::IndexSet;
 use std::{
-	collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
-	fmt::{self, Display, Formatter},
+	collections::{BTreeSet, HashMap},
 	fs
 };
 
@@ -52,69 +52,57 @@ fn read<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<HashMap<String, Ve
 
 #[derive(Clone, Eq, Hash, PartialEq)]
 struct State {
-	previous: Option<String>,
 	vertex: String,
-	remaining: u32,
 	flow_rate: u32,
 	pressure: u32,
 	open: BTreeSet<String>
-}
-
-fn dfs(
-	vertices: &HashMap<String, Vertex>,
-	max_open_vertices: usize,
-	visited: &mut HashSet<State>,
-	mut state: State
-) -> u32 {
-	visited.insert(state.clone());
-	if state.remaining == 0 {
-		return state.pressure;
-	}
-	state.pressure += state.flow_rate;
-	if state.open.len() == max_open_vertices {
-		state.remaining -= 1;
-		return dfs(vertices, max_open_vertices, visited, state);
-	}
-
-	let mut max = 0;
-	let vertex = &vertices[&state.vertex];
-	if !state.open.contains(&state.vertex) && vertex.flow_rate > 0 {
-		let mut state = state.clone();
-		state.previous = None;
-		state.open.insert(state.vertex.clone());
-		state.flow_rate += vertex.flow_rate;
-		state.remaining -= 1;
-		if !visited.contains(&state) {
-			max = max.max(dfs(vertices, max_open_vertices, visited, state));
-		}
-	}
-	for next in &vertex.adj {
-		if state.previous.as_ref() == Some(next) {
-			continue;
-		}
-		let mut state = state.clone();
-		state.vertex = next.into();
-		state.remaining -= 1;
-		if !visited.contains(&state) {
-			max = max.max(dfs(vertices, max_open_vertices, visited, state));
-		}
-	}
-	max
 }
 
 fn main() -> anyhow::Result<()> {
 	let vertices = read("input.txt")?;
 	let max_open_vertices = vertices.values().filter(|v| v.flow_rate > 0).count();
 
-	let mut visited = Default::default();
-	let max = dfs(&vertices, max_open_vertices, &mut visited, State {
-		previous: None,
+	let mut remaining = 30;
+	let mut q = IndexSet::new();
+	let mut next = IndexSet::new();
+
+	q.insert(State {
 		vertex: "AA".into(),
-		remaining: 30,
 		flow_rate: 0,
 		pressure: 0,
-		open: BTreeSet::new()
+		open: Default::default()
 	});
+
+	while remaining > 0 {
+		println!(" remaining: {remaining}, q: {}", q.len());
+		while let Some(mut state) = q.pop() {
+			state.pressure += state.flow_rate;
+			if state.open.len() == max_open_vertices {
+				next.insert(state);
+				continue;
+			}
+
+			let vertex = &vertices[&state.vertex];
+			if !state.open.contains(&state.vertex) && vertex.flow_rate > 0 {
+				let mut state = state.clone();
+				state.open.insert(state.vertex.clone());
+				state.flow_rate += vertex.flow_rate;
+				next.insert(state);
+			}
+
+			for v in &vertex.adj {
+				let mut state = state.clone();
+				state.vertex = v.to_owned();
+				next.insert(state);
+			}
+		}
+		assert!(q.is_empty());
+		q = next;
+		next = IndexSet::new();
+		remaining -= 1;
+	}
+
+	let max = q.iter().map(|state| state.pressure).max().unwrap();
 	println!("{max}");
 
 	Ok(())
